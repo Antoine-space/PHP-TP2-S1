8f16b36914fd680ce7170e9d8063b408286df2ef
Antoine ERNEST <antoine.ernest@etudiant.univ-reims.fr>
2019-02-15 11:18:27 +0100

    Question 3 exe 1

--- testCountry.php
+++ testCountry.php
@@ -6,6 +6,16 @@ require "country.class.php";
 
 //Q2
 $france = new Country ("France", 641185.0 , 66600000);
+$france -> print();
 
-print $france;
+// L'appel de print affiche le nom des paramètres et leur valeur puis le destructeur détruit france
+// Nom : str 
+// surface : float 
+// Population : int
+//Le constructeur sert à attribuer des valeurs aux attributs d'instances de $france.
 
+//Q3
+
+$italie = new Country ("Italie");
+
+$italie -> print();
